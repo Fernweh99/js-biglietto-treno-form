343ae850be5d0e211f571ac1479b8b1289abcf21
Fernweh99 <daniele.dimento@gmail.com>
2022-06-22 16:10:12 +0200

    ex finish with out grafic and validation

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
 // Richiamo ciò che mi serve
+// Bottoni
 const genButton = document.getElementById("generate");
 const resButton = document.getElementById("restore");
-
-// Dichiaro le variabili
-let inputUserName = document.getElementById("user-name");
-let inputKmsTo = document.getElementById("trip-kms");
-let inputAgeRange = document.getElementById("age-range");
+// Input
+const inputUserName = document.getElementById("user-name");
+const inputKmsTo = document.getElementById("trip-kms");
+const inputAgeRange = document.getElementById("age-range");
+// Paragrafi
+let phUserNameTicket = document.getElementById("ticket-username");
+const phOfferTicket = document.getElementById("ticket-offer");
+const phWagonTicket = document.getElementById("ticket-wagon");
+const phCodTicket = document.getElementById("ticket-cod");
+const phPriceTicket = document.getElementById("ticket-price");
+//section
+const yourTicket = document.getElementById("your-ticket")
 
 genButton.addEventListener("click", function(){
   const userName = inputUserName.value;
   const kmsTo = inputKmsTo.value;
   const ageRange = inputAgeRange.value;
+  let price = 0.21 * kmsTo;
+  
+  if(ageRange === "minorenne") price *= 0.8;
+  
+  else if(ageRange === "over65") price *= 0.6;
+
+  phUserNameTicket.innerText = (userName);
+  phOfferTicket.innerText = ("standard");
+  phWagonTicket.innerText = ("5");
+  phCodTicket.innerText = ("91234");
+  phPriceTicket.innerText = (price).toFixed(2) + "€";
+
+  yourTicket.classList.add("d-block");
+
 })
 
 resButton.addEventListener("click", function(){
   inputUserName.value = "";
   inputKmsTo.value = "";
   inputAgeRange.value = "";
+
+  yourTicket.classList.remove("d-block")
 })
